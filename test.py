77adc23b492c9fe61ae8076bbd5e910fa6da46b9
Woo-Ri Ko <wrko@etri.re.kr>
2022-07-05 09:07:33 +0900

    minor change

--- test.py
+++ test.py
@@ -22,32 +22,32 @@ hp.b_use_noise = False
 test_files = open(os.path.join(path.lstm_model, 'files_for_test.csv'), 'r').read().splitlines()
 air_files = glob.glob(os.path.join(path.air_extracted, '*.npz'))
 
-# # uncertainty of train dataset
-# test_dataset = HARDataSet(data_path=path.air_extracted, data_files=test_files, b_pbar=False)
-# test_data_loader = data.DataLoader(test_dataset, batch_size=hp.batch_size, shuffle=True, drop_last=True)
-# acc, uncertainty = model.test(epoch, test_data_loader)
-# print(f'1: accuracy\t{acc:0.4f}\tuncertainty\t{uncertainty:0.4f}')
-#
-# # uncertainty of other action in test dataset
-# test_files_to_compare = [file for file in air_files if 'A001' in file]
-# compare_dataset = HARDataSet(data_path=path.air_extracted, data_files=test_files_to_compare, b_pbar=False)
-# compare_data_loader = data.DataLoader(compare_dataset, batch_size=hp.batch_size, shuffle=True, drop_last=True)
-# acc, uncertainty = model.test(epoch, compare_data_loader)
-# print(f'2: accuracy\t{acc:0.4f}\tuncertainty\t{uncertainty:0.4f}')
-#
-# # uncertainty of other action in test dataset
-# test_files_to_compare = [file for file in air_files if 'A004' in file]
-# compare_dataset = HARDataSet(data_path=path.air_extracted, data_files=test_files_to_compare, b_pbar=False)
-# compare_data_loader = data.DataLoader(compare_dataset, batch_size=hp.batch_size, shuffle=True, drop_last=True)
-# acc, uncertainty = model.test(epoch, compare_data_loader)
-# print(f'3: accuracy\t{acc:0.4f}\tuncertainty\t{uncertainty:0.4f}')
-#
-# # uncertainty of other action in test dataset
-# test_files_to_compare = [file for file in air_files if 'A006' in file]
-# compare_dataset = HARDataSet(data_path=path.air_extracted, data_files=test_files_to_compare, b_pbar=False)
-# compare_data_loader = data.DataLoader(compare_dataset, batch_size=hp.batch_size, shuffle=True, drop_last=True)
-# acc, uncertainty = model.test(epoch, compare_data_loader)
-# print(f'4: accuracy\t{acc:0.4f}\tuncertainty\t{uncertainty:0.4f}')
+# uncertainty of train dataset
+test_dataset = HARDataSet(data_path=path.air_extracted, data_files=test_files, b_pbar=False)
+test_data_loader = data.DataLoader(test_dataset, batch_size=hp.batch_size, shuffle=True, drop_last=True)
+acc, uncertainty = model.test(epoch, test_data_loader)
+print(f'1: accuracy\t{acc:0.4f}\tuncertainty\t{uncertainty:0.4f}')
+
+# uncertainty of other action in test dataset
+test_files_to_compare = [file for file in air_files if 'A001' in file]
+compare_dataset = HARDataSet(data_path=path.air_extracted, data_files=test_files_to_compare, b_pbar=False)
+compare_data_loader = data.DataLoader(compare_dataset, batch_size=hp.batch_size, shuffle=True, drop_last=True)
+acc, uncertainty = model.test(epoch, compare_data_loader)
+print(f'2: accuracy\t{acc:0.4f}\tuncertainty\t{uncertainty:0.4f}')
+
+# uncertainty of other action in test dataset
+test_files_to_compare = [file for file in air_files if 'A004' in file]
+compare_dataset = HARDataSet(data_path=path.air_extracted, data_files=test_files_to_compare, b_pbar=False)
+compare_data_loader = data.DataLoader(compare_dataset, batch_size=hp.batch_size, shuffle=True, drop_last=True)
+acc, uncertainty = model.test(epoch, compare_data_loader)
+print(f'3: accuracy\t{acc:0.4f}\tuncertainty\t{uncertainty:0.4f}')
+
+# uncertainty of other action in test dataset
+test_files_to_compare = [file for file in air_files if 'A006' in file]
+compare_dataset = HARDataSet(data_path=path.air_extracted, data_files=test_files_to_compare, b_pbar=False)
+compare_data_loader = data.DataLoader(compare_dataset, batch_size=hp.batch_size, shuffle=True, drop_last=True)
+acc, uncertainty = model.test(epoch, compare_data_loader)
+print(f'4: accuracy\t{acc:0.4f}\tuncertainty\t{uncertainty:0.4f}')
 
 # uncertainty of same action (handshake) in other dataset
 hp.step = 2
